6ea8c8c9f7bee59b123f5334988c1bb319c6f100
vadxq <dxl@vadxq.com>
2022-12-05 06:38:13 +0800

    feat: lint

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
@@ -53,5 +53,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   experimental: {
     appDir: true,
-  },
-}
+    runtime: 'experimental-edge'
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
